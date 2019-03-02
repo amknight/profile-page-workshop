86d939c7001ba8fa451d1a848ff8710d9b1d5e88
Alex Knight <aknight@atlassian.com>
2019-03-02 17:59:44 +1100

    Remove cruddy comments

--- src/components/ProfileBody.js
+++ src/components/ProfileBody.js
@@ -4,40 +4,6 @@ import Tabs from './TabbedDisplay/Tabs.js'
 import '../styles/ProfileBody.css';
 
 class ProfileBody extends Component {
-  // resizeGridItem (item) {
-  //   let grid = document.getElementsByClassName("tab-content")[0];
-  //   let rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-  //   let rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-  //   let rowSpan = Math.ceil((item.querySelector('.profile-card').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
-  //     item.style.gridRowEnd = "span "+rowSpan;
-  // }
-  
-  // resizeAllGridItems () {
-  //   let allItems = document.getElementsByClassName("profile-card");
-  //   for(let x=0;x<allItems.length;x++){
-  //     this.resizeGridItem(allItems[x]);
-  //   }
-  // }
-  
-  // resizeInstance (instance) {
-  //   let item = instance.elements[0];
-  //   this.resizeGridItem(item);
-  // }
-  
-  // componentDidMount (prevProps) {
-  //   this.resizeAllGridItems();
-  //   this.resizeListener = window.addEventListener("resize", this.resizeAllGridItems);
-    
-  //   // this.allItems = document.getElementsByClassName("item");
-  //   // for(let x=0;x<allItems.length;x++){
-  //   //   imagesLoaded( allItems[x], resizeInstance);
-  //   // }
-  // }
-
-  // componentWillUnmount () {
-  //   window.removeEventListener('resize', this.resizeListener);
-  // }
-
   render() {
     const bodyData = {
       tabList: Object.keys(this.props),
