2ed40e92183e09e595fd8fdf44ad57520f28bca2
Alex Knight <aknight@atlassian.com>
2019-03-11 11:13:59 +1100

    Task 1: Wiring up ProfileBody

--- src/App.js
+++ src/App.js
@@ -2,30 +2,18 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import userData from './UserData.js';
 import ProfileHeader from './components/ProfileHeader.js';
-import ProfileBody from './components/ProfileBody.js';
-import DarkModeToggle from './components/DarkModeToggle.js';
 
 class App extends Component {
   state = {
     userData: userData,
-    isDarkMode: false
   };
 
-  toggleDarkMode() {
-    const currentDarkModeState = this.state.isDarkMode;
-    this.setState({ isDarkMode: !currentDarkModeState });
-  }
-
   render() {
-    const darkModeClass = this.state.isDarkMode ? 'dark' : 'light';
-
     return (
-      <div className={'App-wrapper ' + darkModeClass}>
+      <div className={'App-wrapper light'}>
         <div className="App">
-          {/* Must bind the function to this context before passing as props to component, see: https://reactjs.org/docs/faq-functions.html */}
-          <DarkModeToggle isDarkMode={this.state.isDarkMode} toggleDarkMode={this.toggleDarkMode.bind(this)}></DarkModeToggle>
           <ProfileHeader {...this.state.userData} />
-          <ProfileBody allDetails={this.state.userData.details} />
+          {/* ProfileBody takes a prop "allDetails" which is an array of the "details" objects (see UserData.js) /> */}
         </div>
       </div>
     );
